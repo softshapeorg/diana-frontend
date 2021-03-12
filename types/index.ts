1e7c16d99fc339e7b8e48912093ec62dc9fcad36
Ali Mehasin <alimehasin@gmail.com>
2021-03-12 20:14:55 +0300

    Define more types

--- types/index.ts
+++ types/index.ts
@@ -1,1 +1,21 @@
 export * from "./redux";
+
+interface User {
+  authed: boolean;
+  tokens: UserTokens;
+  data: UserData;
+}
+
+interface UserTokens {
+  access: string;
+  refresh: string;
+}
+
+interface UserData {
+  firstName: string;
+  lastName: string;
+  username: string;
+  email: string;
+}
+
+export type { User, UserTokens, UserData };
--- types/redux.ts
+++ types/redux.ts
@@ -1,3 +1,20 @@
 interface State {}
 
-export type { State };
+interface UserActionTypes {
+  // Login
+  LOGIN_REQUESTED: string;
+  LOGIN_SUCCEED: string;
+  LOGIN_FAILED: string;
+
+  // Registration
+  REGISTRATION_REQUESTED: string;
+  REGISTRATION_SUCCEED: string;
+  REGISTRATION_FAILED: string;
+
+  // Update user
+  UPDATE_REQUESTED: string;
+  UPDATE_SUCCEED: string;
+  UPDATE_FAILED: string;
+}
+
+export type { State, UserActionTypes };
