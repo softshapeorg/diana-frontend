36e4e85313de02ad5a2e546ab48e270d8af15872
Ali Mehasin <alimehasin@gmail.com>
2021-03-13 16:08:32 +0300

    Add logout functionaility

--- pages/accounts/logout.tsx
+++ pages/accounts/logout.tsx
@@ -1,11 +1,22 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import { Container } from "react-bootstrap";
+
+import { cookies } from "../../utils";
 
 interface LogoutProps {}
 
 const Logout: React.FC<LogoutProps> = (props) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    cookies.removeAuthCookies();
+    router.push("/");
+  }, []);
+
   return (
     <Container fluid>
-      <h1>Logout page</h1>
+      <h1>Logging out...</h1>
     </Container>
   );
 };
